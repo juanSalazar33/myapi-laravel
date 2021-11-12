f0f2a79439f2374566d9609b576df8686b0eab95
juancho <juancrogollo@gmail.com>
2021-11-12 15:32:20 -0400

    add CORS for localhost

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        "http://localhost:3000",
+        "localhost",
+        "localhost:3000",
+        "*"
+    ],
 
     'allowed_origins_patterns' => [],
 
