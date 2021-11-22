6ab97da76cc142cbd8c9971b39004b61b9f36d2f
juancho <juancrogollo@gmail.com>
2021-11-21 20:16:05 -0400

    add factory and seeder

---
+++ database/factories/TaskManagementFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskManagementFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+
+        return [
+            'text' => $this->faker->paragraph($nbSentences = 1),
+            'completed' => 0
+        ];
+    }
+}
